cc73f23e97101a46b3bae912ba1622a107af1e89
Rahmat Hidayat <rh.hie.dayatt@gmail.com>
2021-01-09 17:14:23 +0700

    add validator + add index list llk

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,30 +31,46 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-        $user = new User();
-        $path = 'uploads/users/';
+        $validator = Validator::make($request->all(), [
+            'fullname'   => 'required',
+            'mip'   => 'required',
+            'email'  => 'required',
+            'password' => 'required',
+            'jabatan' => 'required',
+        ]);
 
-
-        $user->name = $request->input('fullname');
-        $user->nip = $request->input('nip');
-        $user->email = $request->input('email');
-        $user->password = Hash::make($request->input('password'));
-        $user->jabatan = $request->input('jabatan');
-
-        if($request->hasfile('file') ) {
-            if(!File::exists($path)) {
-                File::makeDirectory($path, $mode = 0777, true, true);
-            }
-
-            $image = $request->file('file');
-            $filename = time() . '.' . $image->getClientOriginalExtension();
-            \Image::make($image)->save( public_path($path . $filename ) );
-            $user->foto = env('APP_URL') . '/uploads/users/' . $filename;
-        } else {
-            $user->foto = '';
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
         }
 
-        $user->save();
+        if ($validator->fails()) {
+            return Redirect::to('auth.register')
+            ->withErrors($validator);
+        } else {
+            $user = new User();
+            $path = 'uploads/users/';
+            
+            $user->name = $request->input('fullname');
+            $user->nip = $request->input('nip');
+            $user->email = $request->input('email');
+            $user->password = Hash::make($request->input('password'));
+            $user->jabatan = $request->input('jabatan');
+            
+            if($request->hasfile('file') ) {
+                if(!File::exists($path)) {
+                    File::makeDirectory($path, $mode = 0777, true, true);
+                }
+                
+                $image = $request->file('file');
+                $filename = time() . '.' . $image->getClientOriginalExtension();
+                \Image::make($image)->save( public_path($path . $filename ) );
+                $user->foto = env('APP_URL') . '/uploads/users/' . $filename;
+            } else {
+                $user->foto = '';
+            }
+            
+            $user->save();
+        }
 
         return response()->json(['status'=>'success']);
     }
--- app/Http/Controllers/ellkController.php
+++ app/Http/Controllers/ellkController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\LembarKerja;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\Console\Input\Input;
@@ -21,8 +22,8 @@ class ellkController extends Controller
         $lks = DB::table('lembar_kerjas')
         ->orderBy('created_at','desc')
         ->get();
-        
-        return view('ellk', compact("lks"));  
+
+        return view('ellk', compact("lks"));
     }
 
     public function create(Request $request)
